e7cf97d6b256eea5588e67977b5507d0dd9d4cb2
kawilkinson <kyle.wilkinson.tech@gmail.com>
2025-06-10 17:29:30 -0400

    initial commit

---
+++ src/main/java/com/kwilkinson/springcart/repositories/CategoryRepository.java
@@ -0,0 +1,4 @@
+package com.kwilkinson.springcart.repositories;
+
+public interface CategoryRepository {
+}
---
+++ src/test/java/com/kwilkinson/springcart/SpringcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwilkinson.springcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
