ab50790a67e26c586aa4e59b3769fa863c4c3992
kawilkinson <kyle.wilkinson.tech@gmail.com>
2025-06-11 21:07:50 -0400

    add spring security and add authentication with login endpoint

--- src/main/java/com/kwilkinson/springcart/repositories/UserRepository.java
+++ src/main/java/com/kwilkinson/springcart/repositories/UserRepository.java
@@ -3,6 +3,11 @@ package com.kwilkinson.springcart.repositories;
 import com.kwilkinson.springcart.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
