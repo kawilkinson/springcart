139bc81609f75f1686f8fd98f5e90b4274ace5f6
kawilkinson <kyle.wilkinson.tech@gmail.com>
2025-06-11 20:00:54 -0400

    create endpoints and logic for shopping cart API

---
+++ src/main/java/com/kwilkinson/springcart/exceptions/CartNotFoundException.java
@@ -0,0 +1,4 @@
+package com.kwilkinson.springcart.exceptions;
+
+public class CartNotFoundException extends RuntimeException {
+}
---
+++ src/main/java/com/kwilkinson/springcart/exceptions/ProductNotFoundException.java
@@ -0,0 +1,4 @@
+package com.kwilkinson.springcart.exceptions;
+
+public class ProductNotFoundException extends RuntimeException {
+}
