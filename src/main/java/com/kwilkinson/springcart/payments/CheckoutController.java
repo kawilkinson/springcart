855f30486cf56f58ff16f596f95a5bf514b4ca8d
kawilkinson <kyle.wilkinson.tech@gmail.com>
2025-06-13 19:03:42 -0400

    add ability to get stripe payments via code into single package

--- src/main/java/com/kwilkinson/springcart/payments/CheckoutController.java
+++ src/main/java/com/kwilkinson/springcart/payments/CheckoutController.java
@@ -1,25 +1,42 @@
-package com.kwilkinson.springcart.controllers;
+package com.kwilkinson.springcart.payments;
 
-import com.kwilkinson.springcart.dtos.CheckoutRequest;
-import com.kwilkinson.springcart.dtos.CheckoutResponse;
 import com.kwilkinson.springcart.dtos.ErrorDto;
 import com.kwilkinson.springcart.exceptions.CartEmptyException;
 import com.kwilkinson.springcart.exceptions.CartNotFoundException;
-import com.kwilkinson.springcart.services.CheckoutService;
+import com.kwilkinson.springcart.repositories.OrderRepository;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@AllArgsConstructor
+import java.util.Map;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/checkout")
 public class CheckoutController {
     private final CheckoutService checkoutService;
+    private final OrderRepository orderRepository;
 
     @PostMapping
     public CheckoutResponse checkout(@Valid @RequestBody CheckoutRequest request) {
         return checkoutService.checkout(request);
+    }
+
+    @PostMapping("/webhook")
+    public void handleWebhook(
+            @RequestHeader Map<String, String> headers,
+            @RequestBody String payload
+    ) {
+        checkoutService.handleWebhookEvent(new WebhookRequest(headers, payload));
+    }
+
+    @ExceptionHandler(PaymentException.class)
+    public ResponseEntity<?> handlePaymentException() {
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ErrorDto("Error creating a checkout session"));
     }
 
     @ExceptionHandler({CartNotFoundException.class, CartEmptyException.class})
---
+++ src/main/java/com/kwilkinson/springcart/payments/PaymentGateway.java
@@ -0,0 +1,11 @@
+package com.kwilkinson.springcart.payments;
+
+import com.kwilkinson.springcart.entities.Order;
+
+import java.util.Optional;
+
+public interface PaymentGateway {
+    CheckoutSession createCheckoutSession(Order order);
+
+    Optional<PaymentResult> parseWebhookRequest(WebhookRequest request);
+}
