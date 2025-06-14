98a24e10ea75d6110aa8974faf99976cbde2c110
kawilkinson <kyle.wilkinson.tech@gmail.com>
2025-06-13 21:52:24 -0400

    add migration to populate database in Railway

---
+++ src/main/resources/db/migration/V5__populate_database.sql
@@ -0,0 +1,22 @@
+INSERT INTO categories (name)
+VALUES ('Fruits'),
+       ('Vegetables'),
+       ('Bakery'),
+       ('Beverages'),
+       ('Snacks'),
+       ('Meat'),
+       ('Seafood'),
+       ('Frozen'),
+       ('Pantry');
+
+INSERT INTO products (name, price, description, category_id)
+VALUES ('Honeycrisp Apples (1 lb)', 3.99, 'Crisp, sweet, and juicy apples, perfect for snacking.', 1),
+       ('Organic Baby Spinach (5 oz)', 4.49, 'Fresh organic spinach leaves, great for salads and smoothies.', 2),
+       ('Whole Milk (1 Gallon)', 3.29, 'Grade A pasteurized whole milk, rich and creamy.', 3),
+       ('Sourdough Bread (Loaf)', 5.49, 'Freshly baked sourdough bread with a tangy flavor and crusty exterior.', 4),
+       ('Coca-Cola (12 pack)', 7.99, '12-pack of 12 fl oz Coca-Cola cans, classic taste.', 5),
+       ('Lay\'s Classic Potato Chips (Family Size)', 4.29, 'Crispy, salted potato chips in a family-size bag.', 6),
+('Boneless Chicken Breasts (1 lb)', 5.99, 'Fresh, skinless, boneless chicken breast fillets.', 7),
+('Atlantic Salmon Fillets (1 lb)', 12.99, 'Fresh Atlantic salmon fillets, sustainably sourced.', 8),
+('Frozen Mixed Vegetables (16 oz)', 2.49, 'A blend of frozen carrots, peas, corn, and green beans.', 9),
+(' Uncle Ben\'s Long Grain Rice (5 lb)', 6.49, 'Enriched long grain white rice, cooks up fluffy every time.', 10);
